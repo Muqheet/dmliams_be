fea1d6a4ebf86d30a0bcf402f858b796ae64bca3
Muqheet <muqeeth246@gmail.com>
2018-02-06 15:45:26 +0530

    fix2

--- src/main/java/org/alimdaadsociety/model/service/AppUserService.java
+++ src/main/java/org/alimdaadsociety/model/service/AppUserService.java
@@ -15,8 +15,10 @@ import org.alimdaadsociety.utility.SmtpMaliSender;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class AppUserService {
 	
 	@Autowired
